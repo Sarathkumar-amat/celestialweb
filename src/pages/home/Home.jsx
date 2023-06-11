3143bf918708b265e00250be06c0c024022b1d18
Sarathkumar-amat <poongodinall@gmail.com>
2023-06-11 09:35:39 +0530

    added icons

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,13 +1,14 @@
 import { useContext, useReducer } from "react"
 import { PostContext } from "../../contexts/PostProvider"
+import { PostCard } from "./components/PostCard";
 
 export function Home()
 {
     const {postState} = useContext(PostContext);
     return (<div>
         <h2>Latest Posts</h2>
-        {postState.posts?.map(({content})=>
-        
-        <div>{content}</div>)}
+        {postState.posts?.map(postData=>
+           
+        <div> <PostCard singlePost={postData}/></div>)}
     </div>)
 }
--- src/pages/home/components/PostCard.jsx
+++ src/pages/home/components/PostCard.jsx
@@ -1,7 +1,17 @@
+import "./PostCard.css"
 export function PostCard({singlePost})
 {
-    const {content,user} = singlePost
-    return (<div>
+    const {content,username} = singlePost
+    return (<div className="postCard">
+       <p>{username}</p> 
+        <p>{content}</p>
+       
+        <i class="bi-heart"></i>
+        <i onClick={()=>console.log("hello")} style={{color:"red"}} class="bi-heart-fill"></i>
+        <i class="bi-chat-left"></i>
+        <i class="bi-share"></i>
+        <i class="bi-bookmark"></i>
+        <i class="bi-bookmark-fill"></i>
 
     </div>)
 }
